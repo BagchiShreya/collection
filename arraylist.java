a6f63669f442f19b9ad47ee1212cbb5251bbee88
Shreya Bagchi <106909868+BagchiShreya@users.noreply.github.com>
2024-03-02 19:51:59 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,27 @@
+package arraylist;
+import java.util.ArrayList;
+import java.util.List;
+public class arraylist {
+	public static void main(String args[]) {
+		int i;
+		ArrayList<String> a1=new ArrayList<String>();
+		
+		a1.add("HTML");
+		a1.add("JAVA");
+		a1.add("SQL");
+		a1.add("ANGULAR");
+		
+		System.out.println(a1);
+		
+		int size=a1.size();
+		
+		String arr[]=new String[size];
+		for(i=0;i<size;i++) {
+			arr[i]=a1.get(i);
+			System.out.println(arr[i]);
+		}
+		
+		List<String> nal=a1.subList(1, 3);
+		System.out.println(nal);
+	}
+}
